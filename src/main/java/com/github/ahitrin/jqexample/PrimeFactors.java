5a386759cd6952527049b70dc2f586759c086827
Andrey Hitrin <andrey.hitrin@gmail.com>
2014-09-26 23:58:48 +0600

    Partial solution, part one...

--- src/main/java/com/github/ahitrin/jqexample/PrimeFactors.java
+++ src/main/java/com/github/ahitrin/jqexample/PrimeFactors.java
@@ -11,6 +11,8 @@ public class PrimeFactors
 {
     public static List<Integer> extract(Integer number)
     {
+        if (number % 2 == 0)
+            return Arrays.asList(2, number / 2);
         return Arrays.asList(number);
     }
 }
