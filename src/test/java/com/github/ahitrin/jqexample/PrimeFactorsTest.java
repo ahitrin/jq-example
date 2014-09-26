29c292b9d053ecdba81fff74b018300822e50b82
Andrey Hitrin <andrey.hitrin@gmail.com>
2014-09-26 23:49:42 +0600

    More theories
    Also: extract constant, fix borders

--- src/test/java/com/github/ahitrin/jqexample/PrimeFactorsTest.java
+++ src/test/java/com/github/ahitrin/jqexample/PrimeFactorsTest.java
@@ -2,7 +2,9 @@ package com.github.ahitrin.jqexample;
 
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isIn;
+import static org.hamcrest.core.Every.everyItem;
 import static org.junit.Assume.assumeThat;
 
 import java.util.Arrays;
@@ -21,11 +23,25 @@ import org.junit.runner.RunWith;
 @RunWith(Theories.class)
 public class PrimeFactorsTest
 {
-    @Theory public void primeNumberIsItsOwnFactor(@ForAll @InRange(minInt = 1, maxInt = 50) Integer number) {
-        List<Integer> firstPrimeNumbers = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47);
+    private final List<Integer> firstPrimeNumbers = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47);
+
+    @Theory public void primeNumberIsItsOwnFactor(@ForAll @InRange(minInt = 2, maxInt = 50) Integer number) {
         assumeThat(number, isIn(firstPrimeNumbers));
 
         List<Integer> factors = PrimeFactors.extract(number);
         assertThat(factors, hasItem(number));
+    }
+
+    @Theory public void productOfFactorsShouldBeEqualToNumber(@ForAll @InRange(minInt = 2, maxInt = 50) Integer number) {
+        List<Integer> factors = PrimeFactors.extract(number);
+        Integer product = 1;
+        for (Integer factor: factors)
+            product = product * factor;
+        assertThat(product, is(number));
+    }
+
+    @Theory public void everyFactorShouldBeSimple(@ForAll @InRange(minInt = 2, maxInt = 50) Integer number) {
+        List<Integer> factors = PrimeFactors.extract(number);
+        assertThat(factors, everyItem(isIn(firstPrimeNumbers)));
     }
 }
